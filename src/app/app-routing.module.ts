341c28d23a68806a9a0a1caa7bcc42053da14c9a
StephanieMcClendon <stephaniewillia.sw@gmail.com>
2022-08-11 17:37:49 -0400

    worked on update button functions and created a form for update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
 
 const routes: Routes = [
   {path: "books", component: BookListComponent},
-  {path: "", redirectTo: "books", pathMatch: "full"}
+  {path: "update-book/:id", component:UpdateBookComponent},
+  {path: "", redirectTo: "books", pathMatch: "full"} 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import {HttpClientModule} from "@angular/common/http";
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    UpdateBookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
